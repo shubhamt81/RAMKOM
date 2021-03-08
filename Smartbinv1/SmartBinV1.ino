291a5db6e80d8cbbce01d0c90ca72014a99be5a8
Tirthesh271 <tirtheshsohani@gmail.com>
2021-03-08 17:17:39 +0530

    Added auto-opening feature.

--- Smartbinv1/SmartBinV1.ino
+++ Smartbinv1/SmartBinV1.ino
@@ -3,33 +3,55 @@
 #include <FirebaseFS.h>
 #include <FirebaseJson.h>
 #include <ESP8266WiFi.h>
+#include <Servo.h>
 
 int val = 0;
 FirebaseData firebaseData;
 // Set these to run example.
-#define FIREBASE_HOST ""//Enter the host of ypur project
-#define FIREBASE_AUTH ""//Open your Realtime Database and go to project overview > project settings > Service accounts > Database secrets and copy your Secret and paste in to your FIREBASE_AUTH.
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define FIREBASE_HOST "Host URL"//Enter the host of your project
+#define FIREBASE_AUTH "Auth code"//Open your Realtime Database and go to project overview > project settings > Service accounts > Database secrets and copy your Secret and paste in to your FIREBASE_AUTH.
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "Password"
 
-const int trigPin = 16;//D0
-const int echoPin = 5;//D1
+Servo myservo;
+int pos = 90;
+
+const int trigPin1 = 16;//D0
+const int echoPin1 = 5;//D1
+
+const int trigPin2 = 2;//D4 Or GPIO-2 of nodemcu
+const int echoPin2 = 0;//D3 Or GPIO-0 of nodemcu
+
 
 //Measure level of garbage pile
-int levelMeasure();
+int levelMeasure(int, int);
+void distanceMeasure(int, int);
 
-  void setup() {
+void setup() 
+{
     // put your setup code here, to run once:
-      pinMode(trigPin,OUTPUT);
-      pinMode(echoPin,INPUT);
+      myservo.attach(15);//D8
+      
+      pinMode(trigPin1,OUTPUT);
+      pinMode(echoPin1,INPUT);
+      
+      pinMode(trigPin2, OUTPUT);
+      pinMode(echoPin2, INPUT);
+      
+      myservo.write(pos);
+      
       Serial.begin(9600);
+      
       // connect to wifi.
       WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
       Serial.print("connecting");
-      while (WiFi.status() != WL_CONNECTED) {
+      
+      while (WiFi.status() != WL_CONNECTED) 
+      {
         Serial.print(".");
         delay(500);
       }
+      
       Serial.println();
       Serial.print("connected: ");
       Serial.println(WiFi.localIP());
@@ -38,23 +60,29 @@ int levelMeasure();
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-      long level = levelMeasure();
+      long level = levelMeasure(trigPin1, echoPin1);
+
+      distanceMeasure(trigPin2, echoPin2);
     
-      if (Firebase.setInt(firebaseData, "/data", level)) {    // On successful Write operation, function returns 1  
+      if (Firebase.setInt(firebaseData, "/data", level)) 
+      {    
+        // On successful Write operation, function returns 1  
                Serial.println("Value Uploaded Successfully");
                Serial.println("Height:");
                Serial.println(level);
-      }  
-      else {        
+      } 
+       
+      else 
+      {        
       Serial.println(firebaseData.errorReason());
       }
       delay(1000);
 }
 
-
-int levelMeasure()
+int levelMeasure(int trigPin, int echoPin)
 {
     static int distance;
     static long duration;
@@ -72,4 +100,16 @@ int levelMeasure()
     distance = duration * 0.017; 
  
     return distance; 
+}
+
+void distanceMeasure(int trigPin, int echoPin)
+{
+    long dist = levelMeasure(trigPin, echoPin);
+    Serial.println(dist);
+    if (dist < 27)
+    {
+      myservo.write(pos+160);
+      delay(1000);
+      myservo.write(pos-160);
+    }
 }
